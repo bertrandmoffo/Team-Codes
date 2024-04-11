bcc16765c5b2d2694be9a0f835eef761cfee1050
bertrandmoffo <bertrandmoffo22@gmail.com>
2024-04-11 16:14:09 -0500

    new keypair

--- keypair.tf
+++ keypair.tf
@@ -1,5 +1,5 @@
-resource "aws_key_pair" "moffo-key" {
-    key_name = "moffo-key.pem"
-    public_key = "moffo-key.pem"
+resource "aws_key_pair" "mykeypair" {
+    key_name = "mykeypair"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtTCK5P6beqDBmhOG/MS8fEcyPTT0jGrlaKnbfQ3sUfJ/bRIEMQqr3Bcx/moABpCc/vmyYco/qKKmA+pD09ki7hfodE2GQLzTkwW1NJ55akYZE3n2OFvYoGfN8fUqlC6e1MonKisDGoGmMLEsi6NqPUN/mUDGUDgQjVvHsRRvJjfJn0HVcv6fZyhP6UbBclLS2SfPgV65KNSf0g7wRjBVqK4pRULY5GPB5Bc/6yqM/8fEgN6frGxvNdKbvauRMxxrEOB57+t65bPAvBkyXHTsvlnM1NsJTrsoLN2f1rOKPJ62YSUOiTDMDcx5euoHIz2tme6J3zA3Jh+xZit8wPbTV Bertrand M@anwei"
   
 }
